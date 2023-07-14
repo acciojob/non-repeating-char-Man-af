c69aa20e804f9c51c2c55d0722c18f7f4da236a1
Man-af <131351514+Man-af@users.noreply.github.com>
2023-07-15 01:35:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+function findFirstNonRepeatedChar(str)
+	{
+
+		for(let i=0; i<str.length-1; i++)
+			{
+				let flag=0;
+				for(let j=i+1; j<str.length; j++)
+					{
+						if(str.charAt(i)===str.charAt(j))
+							flag=1;
+					}
+				if (flag===0)
+					return (str.chatAt(i));
+			}
+		return (str.chatAt(str.length-1));
+
+		
+	}
+
+
+
+
+
+module.exports = findFirstNonRepeatedChar;
