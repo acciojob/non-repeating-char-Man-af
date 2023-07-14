7d4c93a12f5bc4d4c6c9dc315e8da94727af5faa
Man-af <131351514+Man-af@users.noreply.github.com>
2023-07-15 01:39:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //your JS code here. If required.
 function findFirstNonRepeatedChar(str)
 	{
-
+          let flag=0;
 		for(let i=0; i<str.length-1; i++)
 			{
-				let flag=0;
+			     flag=0;
 				for(let j=i+1; j<str.length; j++)
 					{
 						if(str.charAt(i)===str.charAt(j))
